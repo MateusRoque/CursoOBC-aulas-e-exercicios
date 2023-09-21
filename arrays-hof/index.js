9ec2017b5e87d1b1723bc6d466dd3300ee3d794d
MateusRoque <mateusroque1406@gmail.com>
2023-09-21 13:58:20 -0300

    att do dia 21/09

---
+++ arrays-hof/index.js
@@ -0,0 +1,49 @@
+const personagens = [
+  { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+  { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+  { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+  { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+  { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+]
+
+// MAP -- cria novo array e nao altera o inicial
+// const nomes = personagens.map((personagem) => {
+//   // console.log(evento)
+//   // nomes.push(personagem.nome)
+//   return personagem.raca === "Orc"
+// })
+
+// console.log(nomes)
+
+// FILTER -- mesma coisa do map porem serve para filtrar
+
+// const personagensOrc = personagens.filter((personagem) => {
+//   return personagem.raca === "Orc"
+// })
+
+// personagensOrc.forEach((personagem) => {
+//   console.log(personagem.nome)
+// })
+
+// REDUCE  
+// const nivelTotal = personagens.reduce((valorAcumulado, personagem) => {
+//   return valorAcumulado + personagem.nivel
+// }, 0)
+
+// console.log(nivelTotal)
+
+
+const racas = personagens.reduce((valorAcumulado, personagem) => {
+  if (valorAcumulado[personagem.raca]){
+    valorAcumulado[personagem.raca].push(personagem)
+  } else {
+    valorAcumulado[personagem.raca] = [personagem]
+  }
+
+  return valorAcumulado
+
+}, {})
+
+console.log(racas)
