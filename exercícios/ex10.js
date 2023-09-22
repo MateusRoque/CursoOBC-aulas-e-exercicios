d10062b6331bbd7a7e10f00297b52c479f1f8e09
Mateus Roque <mateusroque1406@gmail.com>
2023-09-22 13:11:11 -0300

    voltando ao normal

--- exercícios/ex10.js
+++ exercícios/ex10.js
@@ -7,33 +7,28 @@ do{
   switch (option) {
     case "1":
       alert("op 1")
-      console.log("OLA CARALHO")
     break;
     case "2":
       alert("op 2")
-      console.log("OLA CARALHO")
     break;
     case "3":
       alert("op 3")
-      console.log("OLA CARALHO")
+
     break;
     case "4":
       alert("op 4")
-      console.log("OLA CARALHO")
+
     break;
     case "5":
       alert("op 5")
-      console.log("OLA CARALHO")
+
     break;
     case "6":
       alert("Saindo...")
-      console.log("OLA CARALHO")
     break;
 
     default:
       alert("Opção inválida! Tente novamente")
-
-      console.log("OLA CARALHO")
       break;
   }
 }while(option !== "6")
